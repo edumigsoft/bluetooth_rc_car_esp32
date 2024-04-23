ed56405755b246a62fed51875496cdea79770b42
Anderson S Andrade <asa.sousa@gmail.com>
2024-04-22 21:14:25 -0300

    Implemented status led connected bluetooth. Parcial tone buzzer.

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,64 @@
 #define motorRight_P2 12
 #define motorLeft_P1 32
 #define motorLeft_P2 25
+#define statusLed 2
+#define buzzer 33
+
+unsigned long previousMillisLed = 0;
+unsigned long previousMillisBuzzerOn = 0;
+unsigned long previousMillisBuzzerOff = 0;
 
 BluetoothSerial SerialBT;
 char comando;
 char comandoOld = ' ';
+
+void buzzerBlink(bool on)
+{
+  // unsigned long currentMillis = millis();
+  // int interval = 1000;
+
+  // static bool buzzer_on = false;
+  if (on)
+  {
+    tone(buzzer, 1000);
+    // if (currentMillis - previousMillisBuzzerOn >= interval && !buzzer_on)
+    // {
+    //   previousMillisBuzzerOn = currentMillis;
+
+    //   tone(buzzer, 1000); // Send 1KHz sound signal...
+    //   buzzer_on = true;
+    // }
+
+    // if (currentMillis - previousMillisBuzzerOff >= interval && buzzer_on)
+    // {
+    //   previousMillisBuzzerOff = currentMillis;
+
+    //   noTone(buzzer);
+    //   buzzer_on = false;
+    // }
+  }
+  else
+  {
+    noTone(buzzer);
+  }
+}
+
+void statusBlink(bool connected)
+{
+  unsigned long currentMillis = millis();
+  int interval = 100;
+
+  if (connected)
+  {
+    digitalWrite(statusLed, HIGH);
+  }
+  else if (currentMillis - previousMillisLed >= interval)
+  {
+    previousMillisLed = currentMillis;
+
+    digitalWrite(statusLed, !digitalRead(statusLed));
+  }
+}
 
 void rightStop()
 {
@@ -55,6 +109,8 @@ void setup()
   pinMode(motorRight_P2, OUTPUT);
   pinMode(motorLeft_P1, OUTPUT);
   pinMode(motorLeft_P2, OUTPUT);
+  pinMode(statusLed, OUTPUT);
+  pinMode(buzzer, OUTPUT);
 
   Serial.println();
   Serial.println();
@@ -66,6 +122,70 @@ void setup()
 
 void loop()
 {
+  /*
+
+  Forward------------------F
+
+  Back---------------------B
+
+  Left---------------------L
+
+  Right--------------------R
+
+  Forward Left-------------G
+
+  Forward Right------------I
+
+  Back Left----------------H
+
+  Back Right---------------J
+
+  Stop---------------------S
+
+  Front Lights On----------W
+
+  Front Lights Off---------w (lower case)
+
+  Back Lights On-----------U
+
+  Back Lights Off----------u (lower case)
+
+  Horn On------------------V
+
+  Horn Off-----------------v (lower case)
+
+  Extra On-----------------X
+
+  Extra Off----------------x (lower case)
+
+  Speed 0------------------0
+
+  Speed 10-----------------1
+
+  Speed 20-----------------2
+
+  Speed 30-----------------3
+
+  Speed 40-----------------4
+
+  Speed 50-----------------5
+
+  Speed 60-----------------6
+
+  Speed 70-----------------7
+
+  Speed 80-----------------8
+
+  Speed 90-----------------9
+
+  Speed 100----------------q
+
+  Everything OFF-----------D (Tudo desligado)
+
+  */
+
+  statusBlink(SerialBT.connected());
+
   if (SerialBT.available())
   {
     comando = SerialBT.read();
@@ -80,6 +200,20 @@ void loop()
 
     switch (comando)
     {
+    case 'V':
+    { // Horn On
+      buzzerBlink(true);
+
+      Serial.println("V - Horn on / Buzina ligada");
+      break;
+    }
+    case 'v':
+    { // Horn Off
+      buzzerBlink(false);
+
+      Serial.println("V - Horn off / Buzina desligada");
+      break;
+    }
     case 'F':
     { // Forward / Frente
       rightForward();
